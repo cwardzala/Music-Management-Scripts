ab3abddf9dd74136c95f50c8147daf92cb507e0a
Cameron Wardzala <cwardzala@gmail.com>
2012-10-06 12:44:30 -0400

    adding querystring opts

--- iTunes.py
+++ iTunes.py
@@ -5,7 +5,14 @@ import os, os.path, re, io, string, subprocess, urlparse
 print "Content-Type: text/plain;charset=utf-8"
 print
 
-qs = urlparse.parse_qs(os.environ['QUERY_STRING'])
+qs = urlparse.parse_qs(os.environ['QUERY_STRING'], True)
+
+if 'mode' in qs:
+    mode = qs['mode'][0].lower()
+else:
+    mode = 'list'
+
+print mode
 
 for root, dirs, files in os.walk('/Shared Media/Music'):
     if re.search('.itlp', root) == None:
@@ -13,12 +20,14 @@ for root, dirs, files in os.walk('/Shared Media/Music'):
             if re.search('(.mp3|.m4a|.wav)', file):
                 path = str(root + '/' + file)
                 print path + "\n"
-                if qs.mode and qs.mode.lower() == 'update':
-                p = subprocess.Popen(['osascript', '/Library/WebServer/CGI-Executables/addToiTunes.applescript', path], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
                 
-                out, err = p.communicate()
+                if mode != None and mode == 'update':
+                    
+                    p = subprocess.Popen(['osascript', '/Library/WebServer/CGI-Executables/addToiTunes.applescript', path], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
                 
-                if p.returncode:
-                    print 'ERROR:', err
-                else:
-                    print out
+                    out, err = p.communicate()
+                
+                    if p.returncode:
+                        print 'ERROR:', err
+                    else:
+                        print out
